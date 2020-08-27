daa3ab1cc4618cf3cef2b2352f3493caa6b6001d
Chernyshov_Nikita <xpom5566@mail.ru>
2020-08-27 23:56:12 +0300

    solved: 64ms (73.4%) / 13.6 mb(95.4%)

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+class Solution:
+  def isPalindrome(self, x: int) -> bool:
+    if x < 0:
+      return False
+
+    if str(x) == ''.join(reversed(str(x))):
+        return True
+
+    return False
+
 my = Solution()
-n = 0
-trueAns=0
-ans = my.X(n)
+n = 121
+trueAns = True
+ans = my.isPalindrome(n)
 print("ans", ans, ans == trueAns)
+
+# Runtime: 64 ms, faster than 73.40% of Python3 online submissions for Palindrome Number.
+# Memory Usage: 13.6 MB, less than 95.38% of Python3 online submissions for Palindrome Number.
